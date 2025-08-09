8e8c38ad10446696385171389b046cbcdc086dd4
Ryan Hughes <ryan@heyoodle.com>
2025-08-09 22:31:40 +0200

    Symlink all themes

--- install/development/nvim.sh
+++ install/development/nvim.sh
@@ -10,3 +10,13 @@ if ! command -v nvim &>/dev/null; then
   rm -rf ~/.config/nvim/.git
   echo "vim.opt.relativenumber = false" >>~/.config/nvim/lua/config/options.lua
 fi
+
+# Symlink all theme neovim.lua files to nvim plugins directory
+if [ -d ~/.config/omarchy/themes ]; then
+  for theme_dir in ~/.config/omarchy/themes/*/; do
+    if [ -f "${theme_dir}neovim.lua" ]; then
+      theme_name=$(basename "$theme_dir")
+      ln -sf "${theme_dir}neovim.lua" ~/.config/nvim/lua/plugins/theme-${theme_name}.lua
+    fi
+  done
+fi
